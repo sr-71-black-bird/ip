2d8b157785d9ac03d657eef58507e7e14017cff9
Colin <e1355879@u.nus.edu>
2026-02-10 22:44:24 +0800

    Fix style issues for tests

--- src/test/java/christopher/task/TaskListTest.java
+++ src/test/java/christopher/task/TaskListTest.java
@@ -1,16 +1,14 @@
 package christopher.task;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TaskListTest {
-
     private TaskList taskList;
     private Task t1;
 
