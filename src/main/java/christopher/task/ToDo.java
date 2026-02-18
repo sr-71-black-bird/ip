8a5b4ef2125bf6d2df469273d52d59620f5bf42b
Colin <e1355879@u.nus.edu>
2026-02-19 03:43:26 +0800

    Add getDeadline

--- src/main/java/christopher/task/ToDo.java
+++ src/main/java/christopher/task/ToDo.java
@@ -1,5 +1,8 @@
 package christopher.task;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 /**
  * This is a specific type of task
  * It just contains a task body
@@ -7,6 +10,15 @@ package christopher.task;
 public class ToDo extends Task {
     public ToDo(String name) throws WrongInstructionException {
         super(name);
+    }
+
+    /**
+     * Returns the empty option because this task has no deadline
+     *
+     * @return Optional(empty), to be used with orElse (max time)
+     */
+    public Optional<LocalDateTime> getDeadline() {
+        return Optional.empty();
     }
 
     @Override
